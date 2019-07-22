127a15f07949509f0dbebecd84dfc9659474add0
gabriel.chua <gabriel.chua@TRAINING006.optimum.net>
2019-07-22 18:02:45 +0800

    update on 22/7/2019

--- CLT-Batch6/src/coreJavaOOP/Problem11.java
+++ CLT-Batch6/src/coreJavaOOP/Problem11.java
@@ -1,11 +1,46 @@
 package coreJavaOOP;
 
+import java.util.Arrays;
+
 public class Problem11 {
+	
+	public static int removedupe(int arr[], int n) {
+		if (n==0 || n==1) {
+			return n;
+		}
+		
+		int temp []= new int [n];
+		int j = 0;
+		for (int i = 0; i<n-1;i++) {
+			if(arr[i] != arr[i+1]) {
+				temp[j++] = arr[i];
+			}
+		}
+		temp [j++]=arr[n-1];
+		
+		for(int i=0;i<j;i++) {
+			arr[i] = temp[i];
+		}
+		return j;
+	}
 
 	public static void main(String []args) {
+		int arr[] = {1,2,3,1,2,3,4};
+		Arrays.sort(arr);
+		int length = arr.length;
+		length = removedupe(arr,length);
+		//print array
+		for(int i =0; i<length;i++) {
+			if(arr[i] ==4 ) {
+				System.out.print(arr[i]);
+			} else {
+				System.out.print(arr[i]+", ");
+			}
+			}
+		}
 		
 	}
-}
+
 	
 	
 
---
+++ CLT-Batch6/src/coreJavaOOP/Problem11Practice.java
@@ -0,0 +1,46 @@
+package coreJavaOOP;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Problem11Practice {
+	
+	static void toPrint() {
+		int arr[] = {1, 2, 3, 1, 2, 3, 4};
+		Arrays.parallelSort(arr);
+		int length = arr.length;
+		length = removeDupeElement(arr,length);
+		
+		for(int i = 0; i<length ; i++) {
+			if (arr[i] ==4) {
+				System.out.print(arr[i]);
+				break;
+			} System.out.print(arr[i]+", ");
+		}
+	}
+	
+	static int removeDupeElement(int arr[],int n) {
+		if (n == 0 || n == 1) {
+			return n;
+		}
+		
+		int temp[] = new int [n];
+		int j = 0;
+		for (int i = 0; i<n-1;i++) {
+			if (arr[i] != arr[i+1]) {
+				temp[j++] = arr[i];
+			}
+		}
+		
+		temp [j++] = arr[n-1];
+		//changing original array
+		for(int i = 0;i<j; i++) {
+			arr[i] = temp[i];
+		}
+		return j;
+	}
+	
+public static void main(String []args) {
+	toPrint();
+}
+}
