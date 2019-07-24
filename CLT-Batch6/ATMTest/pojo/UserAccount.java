73523b41f736114a71f67e4fbb60638302ed7d04
gabchuaa <40193949+gabchuaa@users.noreply.github.com>
2019-07-25 00:58:53 +0800

    Add files via upload

--- CLT-Batch6/ATMTest/pojo/UserAccount.java
+++ CLT-Batch6/ATMTest/pojo/UserAccount.java
@@ -1,23 +1,43 @@
-package pojo;
+package pojo1;
 
 public class UserAccount {
 	
-	String loginID;
-	int loginPW;
-	
-	public String getLoginID() {
-		return loginID;
-	}
-	public void setLoginID(String loginID) {
-		this.loginID = loginID;
-	}
-	public int getLoginPW() {
-		return loginPW;
-	}
-	public void setLoginPW(int loginPW) {
-		this.loginPW = loginPW;
-	}
-	
-	
+	private String userEmail;
+	private String userPassword;
+	private String securityKey;
+	private int userBalance = 0; //set userBalance default value to 0
 
+	//getter
+	public String getUserEmail() {
+		return userEmail;
+	}
+
+	public String getUserPassword() {
+		return userPassword;
+	}
+
+	public String getSecurityKey() {
+		return securityKey;
+	}
+
+	public int getUserBalance() {
+		return userBalance;
+	}
+
+	//setter
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
+	}
+
+	public void setUserPassword(String userPassword) {
+		this.userPassword = userPassword;
+	}
+
+	public void setSecurityKey(String securityKey) {
+		this.securityKey = securityKey;
+	}
+
+	public void setUserBalance(int userBalance) {
+		this.userBalance = userBalance;
+	}
 }
