357325bd49e84e3b259f468902baacb01e910461
gabchuaa <40193949+gabchuaa@users.noreply.github.com>
2019-07-25 00:57:23 +0800

    Add files via upload

--- CLT-Batch6/ATMTest/application/LoginApp.java
+++ CLT-Batch6/ATMTest/application/LoginApp.java
@@ -1,11 +1,14 @@
 package application;
 
+
 import controller.LoginRequest;
 
 public class LoginApp {
 
 	public static void main (String []args) {
-		LoginRequest toLogin = new LoginRequest();
-		toLogin.sendLoginRequest();
+		LoginRequest myLogin = new LoginRequest();
+		myLogin.sendLoginRequest();
 	}
+
+
 }
