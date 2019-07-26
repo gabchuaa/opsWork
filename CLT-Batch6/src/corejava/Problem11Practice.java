8be01272be601617f739e6037b5d6d15fdfb6700
gabriel.chua <gabriel.chua@TRAINING006.optimum.net>
2019-07-26 17:51:39 +0800

    updated on 26/7/2019 5.50pm

--- CLT-Batch6/src/corejava/Problem11Practice.java
+++ CLT-Batch6/src/corejava/Problem11Practice.java
@@ -5,41 +5,62 @@ import java.util.Scanner;
 
 public class Problem11Practice {
 	
-	static void toPrint() {
-		int arr [] = {1,2,3,1,2,3,4};
-		Arrays.sort(arr);
-		int length = arr.length;
-		Problem11.removeDupeElement(arr, length);
-		for(int i =0;i<length;i++) {
-			if(arr[i] == 4) {
-				System.out.print(arr[i]);
-				break;
-			}
-			System.out.print(arr[i]+", ");
-		
-		
-		}
-}
 	
-	static int removeDupeElement(int arr[],int n) {
-		 int replaceIndex = 0;
-		 int i,j;
-		 for( i = 0;i<n;i++) {
-			 for(j = i+1;j<n;j++) {
-				 if(arr[i]!=arr[j]) {
-					 break;
-				 }
-			 }
-			 arr [replaceIndex++] = arr[i];
-			 i = j-1;
-			 
-	    }
-		 return replaceIndex;}
-	
-		
+	int num;
 	
 	
+	
+	
+	
+	/*
+	 * static void toPrint() { int arr [] = {1,2,3,1,2,3,4}; Arrays.sort(arr); int
+	 * length = arr.length; Problem11.removeDupeElement(arr, length); for(int i
+	 * =0;i<length;i++) { if(arr[i] == 4) { System.out.print(arr[i]); break; }
+	 * System.out.print(arr[i]+", ");
+	 * 
+	 * 
+	 * } }
+	 * 
+	 * static int removeDupeElement(int arr[],int n) { int replaceIndex = 0; int
+	 * i,j; for( i = 0;i<n;i++) { for(j = i+1;j<n;j++) { if(arr[i]!=arr[j]) { break;
+	 * } } arr [replaceIndex++] = arr[i]; i = j-1;
+	 * 
+	 * } return replaceIndex; }
+	 * 
+	 */
+
+
+public int getNum() {
+		return num;
+	}
+
+
+
+
+	public void setNum(int num) {
+		this.num = num;
+	}
+
+
+
+
 public static void main(String []args) {
-	toPrint();
+	/*Integer[] array = new Integer[10000 * 10000];
+	for(Integer name : array) {
+		System.out.println(name);*/
+	Problem11Practice ref1 = new Problem11Practice();
+	Problem11Practice ref2 = new Problem11Practice();
+	
+	System.out.println(ref1.getNum()+" ref1");
+	System.out.println(ref2.getNum()+" ref2");
+	
+	ref1.setNum(10);
+	ref2.setNum(15);
+	System.out.println(ref1.getNum()+" ref1");
+	System.out.println(ref2.getNum()+" ref2");
+
+	
+	}
+
 }
-}
+
